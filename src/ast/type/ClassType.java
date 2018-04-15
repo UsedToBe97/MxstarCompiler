51c6cad8481000730e050656ec1ca2ddea1d9ee0
UsedToBe <835964664@qq.com>
2018-04-15 22:38:05 +0800

    Lest we die unbloomed.

--- src/ast/type/ClassType.java
+++ src/ast/type/ClassType.java
@@ -4,7 +4,7 @@ import utils.Position;
 
 public class ClassType extends Type{
     public String name;
-    public String typename() {return "Class";}
+    public String typename() {return "Class" + name;}
     public ClassType(String _name, Position _pos) {
         name = _name;
         pos = _pos;
